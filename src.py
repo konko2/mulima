64853b33ca7ecc696a2d117fe7447d57fee3a661
konstantin <heymailon@gmail.com>
2021-03-21 18:45:16 -0500

    WIP update track handling

--- src.py
+++ src.py
@@ -32,8 +32,13 @@ class Track:
 
     is_modified: bool
 
+    #TODO: rename? to Track.is(path), Track.is_file(path), Track.file_is_proper_track(path)
+    @classmethod
+    def could_created_from(cls, path: Path) -> bool:
+        return path.suffix.lstrip('.') in cls.AVAILABLE_TRACK_EXTENSIONS
+
     def __init__(self, path: Path):
-        if path.suffix.lstrip('.') not in self.AVAILABLE_TRACK_EXTENSIONS:
+        if not self.could_created_from(path):
             raise ValueError(f"given track has extension '{path.suffix.lstrip('.')}', "
                              f"but should be one of follow: {self.AVAILABLE_TRACK_EXTENSIONS}")
 
@@ -205,43 +210,44 @@ class Track:
             pic.desc = u'cover'
             self.__mutagen_file.add_picture(pic)
 
+    @classmethod
+    def rename(cls, filepath: Path, formatting_pattern: str):
+        """
+        rename track due to it's tags
+        :param filepath: original path to file
+        :param formatting_pattern: Pattern, used to build new track filename.
+        Use names in Track.TAG_ALIASES as keyword parameters for string's format method, to replace it with tag values
+        :return:
+        """
+        track = cls(filepath)
+        tags = {tag: '' if val is None else val for tag, val in track.get_tags().items()}
+
+        filename = formatting_pattern.format(**tags)
+        filepath.rename(filepath.parent + filename + filepath.suffix)
+
 
 class ABCArchive:
     def __init__(self, path: str):
         self.path = Path(path)
 
-    def get_dirs_tracks(self, *, easy) -> typ.Dict[Path, typ.List[mutagen.File]]:
+    def get_dirs_tracks(self) -> typ.Dict[Path, typ.List[Track]]:
         dir_tracks = dict()
-        for root, _, files in os.walk(self.path):
+        for root, _, filenames in os.walk(self.path):
             dir_path = Path(root)
-            tracks = (mutagen.File(dir_path/file_name, easy=easy) for file_name in files)
-            dir_tracks[dir_path] = [t for t in tracks if t is not None]
+            dir_tracks[dir_path] = [Track(dir_path/fn) for fn in filenames
+                                    if Track.could_created_from(dir_path/fn)]
         return dir_tracks
 
     def get_artist_tag_stat(self) -> typ.Dict[str, int]:
         stat = dict()
         for root, _, files in os.walk(self.path):
-            for file_name in files:
-                music_file = mutagen.File(f'{root}/{file_name}', easy=True)
-                if music_file is not None:
-                    tag_val = music_file.tags['artist'][0] if 'artist' in music_file.tags else None
+            for filename in files:
+                filepath = Path(root, filename)
+                if Track.could_created_from(filepath):
+                    tag_val = Track(filepath).get_tag('artist')
                     stat[tag_val] = stat.get(tag_val, 0) + 1
 
         return stat
-
-    @staticmethod
-    def set_track_filename(track: mutagen.File, formatting_pattern: str):
-        filepath = Path(track.filename)
-        filename = formatting_pattern.format(
-            title=track.tags.get('title', ''),
-            artist=track.tags.get('artist', ''),
-            album=track.tags.get('album', ''),
-            date=track.tags.get('date', ''),
-            composer=track.tags.get('composer', ''),
-            tracknumber=track.tags.get('tracknumber', ''),
-            author=track.tags.get('author', '')
-        )
-        filepath.rename(filepath.parent + filename + filepath.suffix)
 
     def update(self):
         raise NotImplementedError
@@ -263,26 +269,28 @@ class ArtistArchive(ABCArchive):
             checking_artist_names = [artist_name.casefold() for artist_name in checking_artist_names]
 
         for root, _, files in os.walk(self.path):
-            for file_name in files:
-                file_path = Path(root, file_name)
-                track = mutagen.File(file_path, easy=True)
-                if track is not None:
+            for filename in files:
+                filepath = Path(root, filename)
+                if Track.could_created_from(filepath):
+                    track = Track(filepath)
+
                     if new_only and self.last_update_time is not None:
-                        file_modification_time = datetime.datetime.fromtimestamp(file_path.stat().st_mtime)
+                        file_modification_time = datetime.datetime.fromtimestamp(filepath.stat().st_mtime)
                         if file_modification_time < self.last_update_time:
                             continue
 
-                    artist_tag = track.tags.get('artist')
-                    if missed_only and artist_tag is not None:
-                        continue
+                    artist_tag_value = track.get_tag('artist')
 
-                    artist_tag_value = '' if artist_tag is None else artist_tag[0]
-                    artist_tag_value = artist_tag_value if self.case_sensitive else artist_tag_value.casefold()
-                    if any(artist_name in artist_tag_value for artist_name in checking_artist_names):
-                        continue
+                    if artist_tag_value is None:
+                        track.set_tag('artist', self.possible_artist_names[0])
+                    elif missed_only is not None:
+                        if not self.case_sensitive:
+                            artist_tag_value = artist_tag_value.casefold()
 
-                    track.tags['artist'] = self.possible_artist_names[0]
-                    track.save()
+                        if any(artist_name in artist_tag_value for artist_name in checking_artist_names):
+                            continue  # for example collaboration of artists, tag could be 'A & B (feat C)'
+
+                        track.set_tag('artist', self.possible_artist_names[0])
 
     def update(self):
         raise NotImplementedError
@@ -297,16 +305,18 @@ class AlbumArchive(ABCArchive):
         self.last_update_time = None
 
     def set_album_tag(self):
-        folded_album_name = self.album_name.casefold()
-        for _, track_list in self.get_dirs_tracks(easy=True):
+        for _, track_list in self.get_dirs_tracks():
             for track in track_list:
-                if 'album' not in track.tags or folded_album_name not in track.tags['album']:
-                    track.tags['album'] = self.album_name
-                    track.save()
+                album_tag_value = track.get_tag('album')
+
+                # album tag name could contain part info (like 'album p.1'), so
+                # finding album name in tag value is proper check than equality
+                if album_tag_value is None or self.album_name not in album_tag_value:
+                    track.set_tag('album', self.album_name)
+                    track.write()
 
     # TODO: rename
     # TODO: remove?
-    # TODO: rm tracktotal
     def get_dirs_tracks_number_and_dirs_tracks_total(self) -> typ.Tuple[
         typ.Dict[Path, typ.Dict[Path, typ.Optional[int]]],
         typ.Dict[Path, typ.Dict[Path, typ.Optional[int]]]
@@ -314,14 +324,20 @@ class AlbumArchive(ABCArchive):
         dir_tracks_number = dict()
         dir_tracks_total = dict()
 
-        for dir_path, track_list in self.get_dirs_tracks(easy=True).items():
+        for dir_path, track_list in self.get_dirs_tracks().items():
             tracks_number = dict()
             tracks_total = dict()
             for track in track_list:
-                file_path = Path(track.filename)
-                tracks_number[file_path] = int(track['tracknumber'][0]) if 'tracknumber' in track.tags else None
-                tracks_total[file_path] = int(track['tracktotal'][0]) if 'tracktotal' in track.tags else None
+                filepath = track.get_path()
+                tracknumber_val = track.get_tag('tracknumber')
+                if tracknumber_val is not None:
+                    tracknumber_val = int(tracknumber_val)
 
+                tracktotal_val = track.get_tag('tracktotal')
+                if tracktotal_val is not None:
+                    tracktotal_val = int(tracktotal_val)
+
+                tracks_number[filepath], tracks_total[filepath] = tracknumber_val, tracktotal_val
             if tracks_number:
                 dir_tracks_number[dir_path] = tracks_number
             if tracks_total:
@@ -345,45 +361,30 @@ class AlbumArchive(ABCArchive):
         )))
 
         if any_pictures:
-            for dir_path, track_list in self.get_dirs_tracks(easy=False).items():
+            for dir_path, track_list in self.get_dirs_tracks().items():
                 cover_candidates_from_this_dir = list(chain(*(
                     dir_path.glob(pattern) for pattern in ordered_cover_fname_patterns
                 )))
 
                 cover_candidates = cover_candidates_from_this_dir + root_cover_candidates + any_pictures
                 cover_path = cover_candidates[0]
-                with PIL.Image.open(cover_path) as cover:
-                    # TODO move to func
-                    if cover.width > self.COVER_SIZE_LIMIT_PX[0] or cover.self.COVER_SIZE_LIMIT_PX[1]:
-                        resize_proportion = min([
-                            self.COVER_SIZE_LIMIT_PX[0] / cover.width,
-                            self.COVER_SIZE_LIMIT_PX[1] / cover.height,
-                            1
-                        ])
-                        new_size = (int(cover.width * resize_proportion),
-                                    int(cover.height * resize_proportion))
+                cover_mimetype = mimetypes.guess_type(cover_path)
+                cover_data = open(cover_path, 'rb').read()
+                # with PIL.Image.open(cover_path) as cover:
+                #     # TODO move to func
+                #     if cover.width > self.COVER_SIZE_LIMIT_PX[0] or cover.self.COVER_SIZE_LIMIT_PX[1]:
+                #         resize_proportion = min([
+                #             self.COVER_SIZE_LIMIT_PX[0] / cover.width,
+                #             self.COVER_SIZE_LIMIT_PX[1] / cover.height,
+                #             1
+                #         ])
+                #         new_size = (int(cover.width * resize_proportion),
+                #                     int(cover.height * resize_proportion))
+                #
+                #         # TODO try image.tumbnail
 
-                        # TODO try image.tumbnail
-
-
-                    for track in track_list:
-                        if isinstance(track, mutagen.mp3.MP3):
-                            track.tags.add_tag(mutagen.id3.APIC(
-                                encoding=3,
-                                mime=mimetypes.guess_type(cover_path)[0],
-                                type=3,
-                                desc=u'cover',
-                                data=cover.tobytes()
-                            ))
-                            track.save()
-                        if isinstance(track, mutagen.flac.FLAC):
-                            pic = mutagen.flac.Picture()
-                            pic.data = cover.tobytes()
-                            pic.mime = mimetypes.guess_type(cover_path)[0]
-                            pic.type = 3
-                            pic.desc = u'cover'
-                            track.add_picture(pic)
-                            track.save()
+                for track in track_list:
+                    track.set_cover(data=cover_data, mimetype=cover_mimetype)
 
     def update(self):
         self.set_album_tag()
@@ -395,6 +396,8 @@ class AlbumArchive(ABCArchive):
             if len(correct_total_values) > 1:
                 for track_path in dirs_tracks_total[directory]:
                     del mutagen.File(track_path, easy=True).tags['total']
+                    # TODO: так вот тут ваще запутался
+                    # нужно решить что делать с треками как с файлами
 
             track_filename_pattern = '{track_name}'
 
